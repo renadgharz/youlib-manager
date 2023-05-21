fab509f7a1dd0c889e69ebe7cf454fa6d9a30f82
renadgharz <renad.gharz@outlook.com>
2023-05-21 15:11:55 -0400

    updated landing page, resolved #9

--- launch/launch.py
+++ launch/launch.py
@@ -1,11 +1,8 @@
 from flask import Blueprint, render_template
 
-launch_blueprint = Blueprint(
-    "launch_blueprint", 
-    __name__,
-    template_folder='templates',
-    static_folder="static"
-    )
+launch_blueprint = Blueprint("launch_blueprint", __name__,
+                             template_folder = 'templates', 
+                             static_folder = "static")
 
 @launch_blueprint.route('/')
 def launch():

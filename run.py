53349e3a33d07f33262c313608f4d0856c75b205
renadgharz <renad.gharz@outlook.com>
2023-06-01 15:34:31 -0400

    moved config settings to config.py, updated .gitignore

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from launch.launch import launch_blueprint
 from auth.auth import auth_blueprint
+from config import config
+
 
 app = Flask(__name__)
+
+app.config.update(config)
+
 app.register_blueprint(launch_blueprint)
 app.register_blueprint(auth_blueprint)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()

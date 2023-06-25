4eb3dbdf7d5fad4c3e1877ecd6e5d9df7716eca4
renadgharz <renad.gharz@outlook.com>
2023-06-25 11:33:02 -0400

    added blueprint prefix and app logging

--- run.py
+++ run.py
@@ -3,13 +3,20 @@ from launch.launch import launch_blueprint
 from auth.auth import auth_blueprint
 from config import config
 
+import logging
+from logging.handlers import RotatingFileHandler
+
 
 app = Flask(__name__)
 
 app.config.update(config)
 
 app.register_blueprint(launch_blueprint)
-app.register_blueprint(auth_blueprint)
+app.register_blueprint(auth_blueprint, url_prefix = "/auth/")
+
+logging_handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
+logging_handler.setLevel(logging.INFO)
+app.logger.addHandler(logging_handler)
 
 
 if __name__ == "__main__":

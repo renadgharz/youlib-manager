2e92154627d760568211b6647e837c27f1121581
renadgharz <renad.gharz@outlook.com>
2023-06-25 11:39:24 -0400

    removed redirect routes

--- auth/auth.py
+++ auth/auth.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template, redirect, url_for
 from auth.auth_forms import SignupForm, LoginForm, PasswordResetRequestForm, PasswordResetForm
 
+
 auth_blueprint = Blueprint('auth_blueprint', __name__,
                            template_folder = "templates",
-                           static_folder = "static")
+                           static_folder = "static",
+                           static_url_path = "/auth/static/")
 
-@auth_blueprint.route("/auth/signup", methods=['GET', 'POST'])
+
+@auth_blueprint.route("/signup", methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
     
@@ -15,7 +18,7 @@ def signup():
     
     return render_template("signup.html", signup_form=form)
 
-@auth_blueprint.route("/auth/login", methods=['GET', 'POST'])
+@auth_blueprint.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     
@@ -25,7 +28,7 @@ def login():
     
     return render_template("login.html", login_form=form)
 
-@auth_blueprint.route("/auth/pwd-reset-request")
+@auth_blueprint.route("/pwd-reset-request")
 def pwd_reset_request():
     form = PasswordResetRequestForm()
 
@@ -36,7 +39,7 @@ def pwd_reset_request():
     
     return render_template("pwd_reset_request.html", pwd_reset_request_form=form)
 
-@auth_blueprint.route("/auth/pwd-reset")
+@auth_blueprint.route("/pwd-reset")
 def pwd_reset():
     form  = PasswordResetForm()
     
@@ -46,18 +49,3 @@ def pwd_reset():
     
     return render_template("pwd_reset.html", pwd_reset_form=form)
 
-@auth_blueprint.route("/login")
-def redir_login():
-    return redirect(url_for("auth_blueprint.login"))
-
-@auth_blueprint.route("/signup")
-def redir_signup():
-    return redirect(url_for("auth_blueprint.signup"))
-
-@auth_blueprint.route("/pwd-reset-request")
-def redir_pwd_reset_request():
-    return redirect(url_for("auth_blueprint.pwd_reset_request"))
-
-@auth_blueprint.route("/pwd-reset")
-def redir_pwd_reset():
-    return redirect(url_for("auth_blueprint.pwd_reset"))
